eccb958cf2f5fcfec44ee65c38d9df35181cb46f
peachspeed@note <peachspeed04@gmail.com>
2024-09-30 23:49:57 +0900

    캐릭터 추가 , 좌우위아래

--- Lecture06_HandlingInputs/move_character_with_key.py
+++ Lecture06_HandlingInputs/move_character_with_key.py
@@ -2,12 +2,12 @@ from pico2d import *
 
 
 open_canvas()
-grass = load_image('grass.png')
-character = load_image('animation_sheet.png')
+grass = load_image('TUK_GROUND.png')
+character = load_image('character_12_12.png')
 
 
 def handle_events():
-    global running, dir
+    global running, dir_x, dir_y
 
     events = get_events()
     for event in events:
@@ -15,32 +15,42 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dir_x += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -= 1
+                dir_x -= 1
             elif event.key == SDLK_LEFT:
-                dir += 1
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 
 running = True
 x = 800 // 2
+y = 90
 frame = 0
-dir = 0
+dir_x = 0
+dir_y = 0
 
 while running:
     clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame*100, 100, 100, 100, x, 90)
+    grass.draw(1280, 1024)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
-    x += dir * 5
+    x += dir_x * 5
+    y += dir_y * 5
     delay(0.05)
 
 close_canvas()
-
